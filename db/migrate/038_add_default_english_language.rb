a266dddd216126d8050de2da3c570fba9a0ffe93
Carter Parks <carter@carter-mbp.local>
2009-02-03 20:52:15 -0700

    added a migration for a default language of english

---
+++ db/migrate/038_add_default_english_language.rb
@@ -0,0 +1,12 @@
+# Adds table necessary to link uploaded files with products.
+#
+class AddTagAndContentNodeTranslations < ActiveRecord::Migration
+  def self.up
+    Language.create(:name => 'English', :iso_abbreviation => 'en')
+  end
+  
+  def self.down
+    Language.find_by_iso_abbreviation('en').destroy
+  end
+
+end
